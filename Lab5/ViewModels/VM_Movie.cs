d5ff4d3b644923a1c46a1ee1052a6868f6cbc98f
igoryen <igor.yentaltsev@gmail.com>
2014-02-20 22:02:32 -0500

    ViewModels/VM_Movie.cs: changed

--- Lab5/ViewModels/VM_Movie.cs
+++ Lab5/ViewModels/VM_Movie.cs
@@ -1,41 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using Lab5.Models;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
-namespace Lab5.ViewModels {
-
-
-  public class MoviesForList {
-
+namespace INT422TestOne.ViewModels {
+  public class MovieBase {
     [Key]
-    public int Id { get; set; }
+    public int MovieId { get; set; }
 
     [Required]
-    //[RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
     public string Title { get; set; }
   }
-
-
-  public class MovieFull : MoviesForList {
-
-    public MovieFull() {
-      this.Genres = new List<GenresForList>();
-    }
-
+  public class MovieFull : MovieBase {
     [Required]
-    //[StringLength(40, MinimumLength = 3)]
     [Display(Name = "Ticket Price")]
     public decimal TicketPrice { get; set; }
 
-    [Required]
-    [StringLength(100, MinimumLength = 3)]
-    [Display(Name = "Name")]
-    public Director Director { get; set; }
+    public DirectorFull Director { get; set; }
 
-    public List<GenresForList> Genres { get; set; }
+    public List<GenreBase> Genres { get; set; }
+
+    public MovieFull() {
+      this.Director = new DirectorFull();
+      this.Genres = new List<GenreBase>();
+
+    }
   }
 
 }
