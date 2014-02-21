42636a9ad76a9aa3ab5a350182dfb53e19a0ddf4
igoryen <igor.yentaltsev@gmail.com>
2014-02-20 21:50:46 -0500

    Controllers/DataContext.cs: changed

--- Lab5/Models/DataContext.cs
+++ Lab5/Models/DataContext.cs
@@ -1,18 +1,20 @@
 ﻿using System.Data.Entity;
 
-namespace Lab5.Models {
+namespace INT422TestOne.Models {
   public class DataContext : DbContext {
     public DataContext() : base("name=DataContext") { }
 
-
+    public DbSet<Movie> Movies { get; set; }
     public DbSet<Director> Directors { get; set; }
     public DbSet<Genre> Genres { get; set; }
-    public DbSet<Movie> Movies { get; set; }
 
-    public System.Data.Entity.DbSet<Lab5.ViewModels.MovieFull> MovieFulls { get; set; }
+    public System.Data.Entity.DbSet<INT422TestOne.ViewModels.MovieBase> MovieBases { get; set; }
 
-    public System.Data.Entity.DbSet<Lab5.ViewModels.DirectorFull> DirectorFulls { get; set; }
+    public System.Data.Entity.DbSet<INT422TestOne.ViewModels.MovieFull> MovieFulls { get; set; }
 
-    public System.Data.Entity.DbSet<Lab5.ViewModels.GenreFull> GenreFulls { get; set; }
+    public System.Data.Entity.DbSet<INT422TestOne.ViewModels.DirectorBase> DirectorBases { get; set; }
+
+    public System.Data.Entity.DbSet<INT422TestOne.ViewModels.GenreBase> GenreBases { get; set; }
+
   }
 }
