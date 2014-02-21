7cdf72ff5a0320f1cfc9010034fc57dc3551557d
igoryen <igor.yentaltsev@gmail.com>
2014-02-20 21:58:57 -0500

    ViewModels/RepositoryBase.cs: changed

--- Lab5/ViewModels/RepositoryBase.cs
+++ Lab5/ViewModels/RepositoryBase.cs
@@ -2,25 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Lab5.Models;
+using INT422TestOne.Models;
 
-namespace Lab5.ViewModels {
-
+namespace INT422TestOne.ViewModels {
   public class RepositoryBase {
-
-    protected DataContext dc;
-
     public RepositoryBase() {
 
       dc = new DataContext();
 
-      // Turn off the Entity Framework (EF) proxy creation features
-      // We do NOT want the EF to track changes - we'll do that ourselves
+      // turn off EF tracking changes and lazy loading
+      //   we do it ourselves
       dc.Configuration.ProxyCreationEnabled = false;
-
-      // Also, turn off lazy loading...
-      // We want to retain control over fetching related objects
       dc.Configuration.LazyLoadingEnabled = false;
+
     }
+
+    // implementation details
+    protected DataContext dc;
   }
 }
